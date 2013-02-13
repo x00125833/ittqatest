82b0e5ee6b01049109e6b8d02bf30c375452a41f
jzburns <jzburns@75fbca60-fcae-9687-1dfc-400cc7fa0dfa>
2013-02-13 20:45:30 +0000

    first C program using arrays

--- trunk/simple.C
+++ trunk/simple.C
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
-//global int not modifiable
 const int n = 1000;
 
-//global int modifiable
-int y = 1000;
+void populateData(int* dataarray, int sz);
 
 int main (int argc, char** argv) {
-//local int modifiable
-  int r = 10;
+
+  int intarray[n];
+
+  populateData(intarray, n);
 
   return 0;
+}
+
+void populateData(int* dataarray, int sz){
+  for(int i = 0; i < sz; i++){
+    dataarray[i] = i;
+  }
+
 }
