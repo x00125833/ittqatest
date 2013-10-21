ed78b48768c175b3fd3927f9fbd06436be81efce
jzburns <jzburns@75fbca60-fcae-9687-1dfc-400cc7fa0dfa>
2013-10-21 09:00:39 +0000

    new version of grades

---
+++ Grades_v2.C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Grades.C
+ */
+int main(int argc, char** argv) {
+  int practical = atoi(argv[1]);
+  int exam = atoi(argv[2]);
+  char grade[100] = "Invalid Input"; 
+    if (exam < 40 && practical < 40) {
+      fprintf(stderr,"Fail\n");  
+    } else {
+      int combined = (60*exam+40*practical)/100; 
+      if (combined < 50) {
+        fprintf(stderr,"Component Fail\n");  
+      } else if (combined >= 50 && combined < 80) {
+        fprintf(stderr,"Pass\n");  
+      } else if (combined >= 80 && combined <= 100) {
+        fprintf(stderr,"Pass with distinction\n");  
+      } else {
+        fprintf(stderr,"Invalid Input\n");  
+      }
+    }
+  return 0;
+}
